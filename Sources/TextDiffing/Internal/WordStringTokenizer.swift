e38280e7f4f0e72c5d0b215843fbb9a300c1377b
Simon Støvring <mail@simonbs.dk>
2025-06-03 09:03:00 +0200

    Removes empty options

--- Sources/TextDiffing/Internal/WordStringTokenizer.swift
+++ Sources/TextDiffing/Internal/WordStringTokenizer.swift
@@ -6,7 +6,7 @@ struct WordStringTokenizer: StringTokenizer {
         let tagger = NSLinguisticTagger(tagSchemes: [.tokenType], options: 0)
         tagger.string = text
         let range = NSRange(location: 0, length: text.utf16.count)
-        tagger.enumerateTags(in: range, unit: .word, scheme: .tokenType, options: []) { _, tokenRange, _ in
+        tagger.enumerateTags(in: range, unit: .word, scheme: .tokenType) { _, tokenRange, _ in
             let word = (text as NSString).substring(with: tokenRange)
             result.append(word)
         }
